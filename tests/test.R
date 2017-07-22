056e255259e9f5b5f5ee1776e5bce845c921fe28
Anthony Damico <ajdamico@gmail.com>
2017-07-22 16:50:22 -0400

    setup automation

--- tests/test.R
+++ tests/test.R
@@ -1,12 +1,77 @@
-machine_specific_replacements <- 
-	list( 
-		
-		# replace the folder path on macnix
-		c( 'path.expand( \"~\" ) , \"PLS\"' , paste0( '"' , getwd() , '"' ) ) ,
-		
-		# change other things in the script to be run
-		c( "hello" , "howdy" )
-	)
 if ( .Platform$OS.type == 'windows' ) memory.limit( 256000 )
 
-source( lodown::syntaxtractor( "pls" , replacements = machine_specific_replacements , setup_test = "test" ) , echo = TRUE )
+pls_df <- readRDS( file.path( getwd() , "2014/pls_fy_ae_puplda.rds" ) )
+
+pls_df <- 
+	transform( 
+		pls_df , 
+		
+		c_relatn = 
+			factor( c_relatn , levels = c( "HQ" , "ME" , "NO" ) ,
+				c( "HQ-Headquarters of a federation or cooperative" ,
+				"ME-Member of a federation or cooperative" ,
+				"NO-Not a member of a federation or cooperative" )
+			) ,
+			
+		more_than_one_librarian = as.numeric( libraria > 1 )
+				
+	)	
+nrow( pls_df )
+
+table( pls_df[ , "stabr" ] , useNA = "always" )
+mean( pls_df[ , "popu_lsa" ] )
+
+tapply(
+	pls_df[ , "popu_lsa" ] ,
+	pls_df[ , "stabr" ] ,
+	mean 
+)
+prop.table( table( pls_df[ , "c_relatn" ] ) )
+
+prop.table(
+	table( pls_df[ , c( "c_relatn" , "stabr" ) ] ) ,
+	margin = 2
+)
+sum( pls_df[ , "popu_lsa" ] )
+
+tapply(
+	pls_df[ , "popu_lsa" ] ,
+	pls_df[ , "stabr" ] ,
+	sum 
+)
+quantile( pls_df[ , "popu_lsa" ] , 0.5 )
+
+tapply(
+	pls_df[ , "popu_lsa" ] ,
+	pls_df[ , "stabr" ] ,
+	quantile ,
+	0.5 
+)
+sub_pls_df <- subset( pls_df , visits > 1000000 )
+mean( sub_pls_df[ , "popu_lsa" ] )
+var( pls_df[ , "popu_lsa" ] )
+
+tapply(
+	pls_df[ , "popu_lsa" ] ,
+	pls_df[ , "stabr" ] ,
+	var 
+)
+t.test( popu_lsa ~ more_than_one_librarian , pls_df )
+this_table <- table( pls_df[ , c( "more_than_one_librarian" , "c_relatn" ) ] )
+
+chisq.test( this_table )
+glm_result <- 
+	glm( 
+		popu_lsa ~ more_than_one_librarian + c_relatn , 
+		data = pls_df
+	)
+
+summary( glm_result )
+library(dplyr)
+pls_tbl <- tbl_df( pls_df )
+pls_tbl %>%
+	summarize( mean = mean( popu_lsa ) )
+
+pls_tbl %>%
+	group_by( stabr ) %>%
+	summarize( mean = mean( popu_lsa ) )
